3b496610efb1a92c6b29cb2d1fee3ad1c3849eb9
Concourse <concourse@finbourne.com>
2023-11-22 15:46:09 +0000

    Updates for v2.0.1

--- sdk/Finbourne.Insights.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Insights.Sdk/Client/Configuration.cs
@@ -543,7 +543,7 @@ namespace Finbourne.Insights.Sdk.Client
             string report = "C# SDK (Finbourne.Insights.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.0.748\n";
+            report += "    Version of the API: 0.0.749\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
