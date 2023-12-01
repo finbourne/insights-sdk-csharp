2ef5c2210e6078ca8c7926bbc557acff93a16827
Concourse <concourse@finbourne.com>
2023-12-01 11:42:05 +0000

    Updates for v2.0.1

--- sdk/Finbourne.Insights.Sdk/Extensions/ApiConfiguration.cs
+++ sdk/Finbourne.Insights.Sdk/Extensions/ApiConfiguration.cs
@@ -6,6 +6,7 @@
  */
 
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace Finbourne.Insights.Sdk.Extensions
 {
@@ -42,6 +43,7 @@ namespace Finbourne.Insights.Sdk.Extensions
         /// <summary>
         /// Finbourne-insights Api Url
         /// </summary>
+        [ConfigurationKeyName("finbourne-insightsUrl")]
         public string BaseUrl { get; set; }
 
         /// <summary>
